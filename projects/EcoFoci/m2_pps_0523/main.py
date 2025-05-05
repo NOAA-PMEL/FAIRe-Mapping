aefba5a467718d2ffffcab1b239d243fb2c141d8
brynnz22 <brynn.zalmanek@noaa.gov>
2025-05-05 14:53:28 -0700

    add m2_pps_0523 functionality

---
+++ projects/EcoFoci/m2_pps_0523/main.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import sys
+sys.path.append("../../..")
+from utils.sample_metadata_mapper import FaireSampleMetadataMapper
+
+#TODO: Still need to flush out mapping file - quite different from others. Shannon will look at it.
+def create_m2_pps_0523_sample_metadata():
+
+    # initiate mapper
+    sample_mapper = FaireSampleMetadataMapper(config_yaml='config.yaml')
+
+    sample_metadata_results = {}
+
+    ###### Add Sample Mappings ######
+    # Step 1: Add exact mappings
+    for faire_col, metadata_col in sample_mapper.mapping_dict[sample_mapper.exact_mapping].items():
+        sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df[metadata_col].apply(
+            lambda row: sample_mapper.apply_exact_mappings(metadata_row=row, faire_col=faire_col))
+    
+    # Step 2: Add constants
+    for faire_col, static_value in sample_mapper.mapping_dict[sample_mapper.constant_mapping].items():
+        sample_metadata_results[faire_col] = sample_mapper.apply_static_mappings(faire_col=faire_col, static_value=static_value)
+
+    # Step 3: Add related mappings
+    for faire_col, metadata_col in sample_mapper.mapping_dict[sample_mapper.related_mapping].items():
+        # Add samp_category
+        if faire_col == 'samp_category' and metadata_col == sample_mapper.sample_metadata_sample_name_column:
+            sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df.apply(
+                lambda row: sample_mapper.add_samp_category_by_sample_name(metadata_row=row, faire_col=faire_col, metadata_col=metadata_col),
+                axis=1
+            )
+        elif faire_col == 'biological_rep_relation':
+            sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df.apply(
+                lambda row: sample_mapper.add_biological_replicates(metadata_row=row, faire_missing_val='not applicable'),
+                axis=1
+            )
+        # elif faire_col == 'materialSampleID' or faire_col == 'sample_derived_from':
+        #     sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df.apply(
+        #         lambda row: sample_mapper.add_material_sample_id(metadata_row=row),
+        #         axis=1
+        #     )
+        elif faire_col == 'minimumDepthInMeters':
+            sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df.apply(
+                lambda row: sample_mapper.convert_min_depth_from_minus_one_meter(metadata_row=row, max_depth_col_name=metadata_col),
+                axis=1
+            )
+        
+        elif faire_col == 'geo_loc_name':
+            sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df.apply(
+                lambda row: sample_mapper.format_geo_loc(metadata_row=row, geo_loc_metadata_col=metadata_col),
+                axis=1
+            )
+        elif faire_col == 'env_local_scale':
+            sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df[metadata_col].apply(sample_mapper.calculate_env_local_scale)
+
+        # elif faire_col == 'prepped_samp_store_dur':
+        #     date_col_names = metadata_col.split(' | ')
+        #     sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df.apply(
+        #         lambda row: sample_mapper.calculate_date_duration(metadata_row=row, start_date_col=date_col_names[0], end_date_col=date_col_names[1]),
+        #         axis=1
+        #     )
+        
+        elif faire_col == 'date_ext':
+            sample_metadata_results[faire_col] = sample_mapper.sample_metadata_df[metadata_col].apply(sample_mapper.convert_date_to_iso8601)
+            
+    # Step 4: fill in NA with missing not collected or not applicable because they are samples
+    sample_df = sample_mapper.fill_empty_sample_values(df = pd.DataFrame(sample_metadata_results))
+    
+    # There are no NC samples int his cruise so skip steps...
+
+    # Step 6: Combine all mappings at once (add nc_df if negative controls were sequenced)
+    faire_sample_df = pd.concat([sample_mapper.sample_faire_template_df, sample_df])
+   
+    # step 7: save to excel file
+    sample_mapper.add_final_df_to_FAIRe_excel(excel_file_to_read_from=sample_mapper.faire_template_file,
+                                              sheet_name=sample_mapper.sample_mapping_sheet_name, 
+                                              faire_template_df=faire_sample_df)
+
+    return sample_mapper
+
+ 
+def main() -> None:
+
+    sample_metadata = create_m2_pps_0523_sample_metadata()
+
+if __name__ == "__main__":
+    main()
+
