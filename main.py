1ea14d94b9c520b57a45cd54b0fc9d6753b24166
brynnz22 <brynn.zalmanek@noaa.gov>
2025-06-03 16:35:28 -0700

    remove main.

--- main.py
+++
@@ -1,4 +0,0 @@
-# AFter running all creaet_Sample_metadata for each cruise and running all create experiment metadata for each sequencing run
-# 1. concat all samples together
-# 2. concat experiment run together
-# 3. Get positives, pcr_extractions, field negatives, etc. and add logic for rel_cont_id, pos_cont_type, and neg_cont_typ (for pcr negatives) for smaple metadata
