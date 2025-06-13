f1b1ec13bca87f8caa7a26d605c52b636c5635cb
Brynn Zalmanek <brynn.zalmanek@noaa.gov>
2025-06-13 10:48:13 -0700

    update sample_metadata model

--- models/sample_metadata.py
+++ models/sample_metadata.py
@@ -4,7 +4,7 @@ from shapely.geometry import Point
 import geopandas as gpd
 import warnings
 from pathlib import Path
-
+ # Field(default=None) is for USer defined fields that might be missing in each csv
 # TODO: Add custom data validation for controlled vocabs that accept 'other: <description>' (samp_category, neg_cont_type, verbatimCoordinateSystem, verbatimSRS,
 # samp_size_unit, samp_store_temp, samp_store_sol, precip_chem_prep, filter_material)
 # TODO: Add list validation abstracted out (sample_derived_from)
@@ -19,8 +19,8 @@ from pathlib import Path
 class SampleMetadata(BaseModel):
     samp_name: str
     samp_category: Literal['sample', 'negative control', 'positive control', 'PCR standard']
-    neg_cont_type: Literal['site negative', 'field negative', 'process negative', 'extraction negative', 'PCR negative']
-    pos_cont_type: str
+    neg_cont_type: Optional[Literal['site negative', 'field negative', 'process negative', 'extraction negative', 'PCR negative']]
+    pos_cont_type: Optional[str]
     materialSampleID: Optional[int]
     sample_derived_from: Optional[str] # needs custom list validation
     sample_composed_of: Optional[str] # needs custom list validation
@@ -40,7 +40,7 @@ class SampleMetadata(BaseModel):
     env_broad_scale: str
     env_local_scale: str
     env_medium: str
-    habitat_natural_artificial_0_1: Optional[Literal['0', '1]']]
+    habitat_natural_artificial_0_1: Optional[Literal[0, 1]]
     samp_collect_method: Optional[str]
     samp_collect_device: Optional[str]
     samp_size: Optional[float]
@@ -52,7 +52,7 @@ class SampleMetadata(BaseModel):
     dna_store_loc: Optional[str]
     samp_store_method_additional: Optional[str]
     samp_mat_process: Optional[str]
-    filter_passive_active_0_1: Optional[Literal['0', '1]']]
+    filter_passive_active_0_1: Optional[Literal[0, 1]]
     stationed_sample_dur: Optional[str]
     pump_flow_rate: Optional[float]
     pump_flow_rate_unit: Optional[Literal['m3/s ', 'm3/min ', 'm3/h', 'L/s', 'L/min', 'L/h']]
@@ -71,10 +71,10 @@ class SampleMetadata(BaseModel):
     prepped_samp_store_sol: Optional[Literal['ethanol', 'sodium acetate', 'longmire', 'lysis buffer']]
     prepped_samp_store_dur: Optional[str]
     prep_method_additional: Optional[str] 
-    assay_name: Literal['ssu18sv9_amaralzettler', 'dloopv1.5v4_baker', 'lsu16s_2434-2571_kelly', 
+    assay_name: Optional[Union[Literal['ssu18sv9_amaralzettler', 'dloopv1.5v4_baker', 'lsu16s_2434-2571_kelly', 
                         'COI_1835-2198_lerayfolmer', 'ssu18sv8_machida', 'ssu18sv8_machida_OSUmod', 
                         'ssu16sv4v5_parada', 'ssu16sv4v5_parada_OSUmod', 'ssu18sv4_stoeck', 
-                        'ITS1_sterling', 'ssu12sv5v6_mifish_u_sales']
+                        'ITS1_sterling', 'ssu12sv5v6_mifish_u_sales'], None]] = None
     samp_weather: Optional[str]
     minimumDepthInMeters: Optional[float]
     maximumDepthInMeters: Optional[float]
@@ -86,7 +86,7 @@ class SampleMetadata(BaseModel):
     ph: Optional[float]
     ph_meth: Optional[str]
     salinity: Optional[float] = Field(description = 'partical salinity unit (psu)')
-    salinity_flag: Optional[int]
+    salinity_flag: Optional[int] = Field(default=None)
     suspend_part_matter: Optional[float] = Field(description = 'in mg/L')
     tidal_stage: Optional[str]
     turbidity: Optional[float] = Field(description = 'nephelometric turbidity unit (ntu)')
@@ -96,7 +96,7 @@ class SampleMetadata(BaseModel):
     wind_speed: Optional[float] = Field(descripton = 'in m/s')
     diss_inorg_carb: Optional[float]
     diss_inorg_carb_unit: Optional[str]
-    diss_inorg_carb_flag: Optional[int]
+    diss_inorg_carb_flag: Optional[int] = Field(default=None)
     diss_inorg_nitro: Optional[float]
     diss_inorg_nitro_unit: Optional[Literal['µM', 'mol/m3', 'mmol/m3', 'µmol/m3 ', 'mol/L', 'mmol/L', 'µmol/L', 'mg/L',  'µg/L', 'µmol/kg', 'mmol/kg ', 'parts per million']]
     diss_org_carb: Optional[float]
@@ -127,10 +127,10 @@ class SampleMetadata(BaseModel):
     part_org_nitro_unit: Optional[Literal['µM', 'mol/m3', 'mmol/m3', 'µmol/m3 ', 'mol/L', 'mmol/L', 'µmol/L', 'mg/L',  'µg/L', 'µmol/kg', 'mmol/kg ', 'parts per million']]
     nitrate: Optional[float]
     nitrate_unit: Optional[Literal['µM', 'mol/m3', 'mmol/m3', 'µmol/m3 ', 'mol/L', 'mmol/L', 'µmol/L', 'mg/L',  'µg/L', 'µmol/kg', 'mmol/kg ', 'parts per million']]
-    nitrate_flag: Optional[str]
+    nitrate_flag: Optional[str] = Field(default=None)
     nitrite: Optional[float]
     nitrite_unit: Optional[Literal['µM', 'mol/m3', 'mmol/m3', 'µmol/m3 ', 'mol/L', 'mmol/L', 'µmol/L', 'mg/L',  'µg/L', 'µmol/kg', 'mmol/kg ', 'parts per million']]
-    nitrite_flag: Optional[int]
+    nitrite_flag: Optional[int] = Field(default=None)
     nitro: Optional[float]
     nitro_unit: Optional[Literal['µM', 'mol/m3', 'mmol/m3', 'µmol/m3 ', 'mol/L', 'mmol/L', 'µmol/L', 'mg/L',  'µg/L', 'µmol/kg', 'mmol/kg ', 'parts per million']]
     org_carb: Optional[float]
@@ -141,28 +141,28 @@ class SampleMetadata(BaseModel):
     org_nitro_unit: Optional[Literal['µM', 'mol/m3', 'mmol/m3', 'µmol/m3 ', 'mol/L', 'mmol/L', 'µmol/L', 'mg/L',  'µg/L', 'µmol/kg', 'mmol/kg ', 'parts per million']]
     ammonium: Optional[float]
     ammonium_unit: Optional[Literal['µmol/L']]
-    ammonium_flag: Optional[int]
+    ammonium_flag: Optional[int] = Field(default=None)
     carbonate: Optional[float]
     carbonate_unit: Optional[Literal['']]  # need to add cv here when known
-    hydrogen_ion: Optional[float]
-    hydrogen_ion_unit: Optional[int]
+    hydrogen_ion: Optional[float] = Field(default=None)
+    hydrogen_ion_unit: Optional[int] = Field(default=None)
     nitrate_plus_nitrite: Optional[float]
     nitrate_plus_nitrite_unit: Optional[Literal['']]  # need to add cv here when known
     omega_arag: Optional[float]
-    omega_calc: Optional[float]
+    omega_calc: Optional[float] = Field(default=None)
     pco2: Optional[float]
     pco2_unit: Optional[Literal['uatm']]
     phosphate: Optional[float]
     phosphate_unit: Optional[Literal['µmol/L']]
-    phosphate_flag: Optional[int]
+    phosphate_flag: Optional[int] = Field(default=None)
     pressure: Optional[float]
     pressure_unit: Optional[Literal['dbar']]
     silicate: Optional[float]
     silicate_unit: Optional[Literal['µmol/L']]
-    silicate_flag: Optional[int]
+    silicate_flag: Optional[int] = Field(default=None)
     tot_alkalinity: Optional[float]
     tot_alkalinity_unit: Optional[Literal['µmol/kg']]
-    tot_alkalinity_flag: Optional[int]
+    tot_alkalinity_flag: Optional[int] = Field(default=None)
     transmittance: Optional[float]
     transmittance_unit: Optional[Literal['']] # need to add cv here when known
     serial_number: Optional[str]
@@ -183,14 +183,14 @@ class SampleMetadata(BaseModel):
     par_unit: Optional[Literal['µmol s-1 m-2']]
     air_pressure_at_sea_level: Optional[float]
     air_pressure_at_sea_level_unit: Optional[Literal['mb']]
-    d18O_permil: Optional[float] = Field(description = '18O oxygen iostope ratio, expressed in per mill (%) unit deviations from the international standard which is Standard Mean Ocean Water, as distributed by the International Atomic Energy Agency.')
-    d18O_permil_flag: Optional[int]
+    d18O_permil: Optional[float] = Field(default=None, description='18O oxygen iostope ratio, expressed in per mill (%) unit deviations from the international standard which is Standard Mean Ocean Water, as distributed by the International Atomic Energy Agency.')
+    d18O_permil_flag: Optional[int] = Field(default=None)
     expedition_id: Optional[str]
     expedition_name: Optional[str]
     rosette_position: Optional[int]
     collected_by: Optional[str]
     meaurements_from: Optional[str]
-    niskin_flag: Optional[int]
+    niskin_flag: Optional[int] = Field(default=None)
 
     
     # class variables loaded once and shared across all datasets
@@ -226,10 +226,13 @@ class SampleMetadata(BaseModel):
     # Treat missing faire field strings as empty pre-validation
     @model_validator(mode='before')
     @classmethod
-    def convert_missing_strings(cls, v: Any) -> Any:
-        if isinstance(v, str) and v in cls._faire_missing_fields:
-            return None
-        return v
+    def convert_missing_strings(cls, data: Any) -> Any:
+        if isinstance(data, dict):
+            # Process each filed in the dictionary
+            for key, value in data.items():
+                if isinstance(value, str) and value in cls._faire_missing_fields:
+                    data[key] = None
+            return data
 
     # validate materialSampleID as only allowed 4 or 6 digit
     @field_validator('materialSampleID')
@@ -283,4 +286,25 @@ class SampleMetadata(BaseModel):
                     warnings.warn(f"{self.samp_name} has lat lon coordinates that point to {supposed_sea}, but geo_loc is listed as {geo_loc_sea_area}, double check this!")
                 break
 
+    @model_validator(mode='after')
+    def validate_neg_cont_type_conditions(self):
+        """Validate that neg_cont_type is required when samp_category is 'negative control"""
+        if self.samp_category == 'negative control':
+            if self.neg_cont_type is None:
+                raise ValueError(f"{self.samp_name}: neg_cont_type is required when samp_category is 'negative control'")
+        return self
     
+    @model_validator(mode='after')
+    def validate_pos_cont_type_conditions(self):
+        """Validate that pos_cont_type is required when samp_category is 'positive control"""
+        if self.samp_category == 'positive control':
+            if self.pos_cont_type is None:
+                raise ValueError(f"{self.samp_name}: pos_cont_type is required when samp_category is 'positive control'")
+        return self
+    
+
+## Notes: without @classmethod you can access self.other_field, but iwth @class_method you cannot. Use @classmethod when you only need to validate a single field.
+## mode='after' runs after all the fields are processed and vliadted
+## Notes: @model_Validator with @class_method mode='before' runs before the model is constructed. your working with data before feild validation. Must use 
+# @class_method because no instance exists yet
+## NOtes @model_validator without @class_method mode='after' runs after the model is fully constructed and vliadted
