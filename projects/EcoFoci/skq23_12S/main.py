59fcd69c17cdac706a636b380867755997ec1c11
Brynn Zalmanek <brynn.zalmanek@noaa.gov>
2025-06-16 14:49:11 -0700

    update skq23_12S

--- projects/EcoFoci/skq23_12S/main.py
+++ projects/EcoFoci/skq23_12S/main.py
@@ -165,9 +165,9 @@ def create_skq23_12s_sample_metadata():
     sample_mapper.save_final_df_as_csv(final_df=faire_sample_df_updated, sheet_name=sample_mapper.sample_mapping_sheet_name, header=2, csv_path='/home/poseidon/zalmanek/FAIRe-Mapping/projects/EcoFoci/skq23_12S/data/skq23_12S_faire.csv')
    
     # step 7: save to excel file
-    sample_mapper.add_final_df_to_FAIRe_excel(excel_file_to_read_from=sample_mapper.faire_template_file,
-                                              sheet_name=sample_mapper.sample_mapping_sheet_name, 
-                                              faire_template_df=faire_sample_df_updated)
+    # sample_mapper.add_final_df_to_FAIRe_excel(excel_file_to_read_from=sample_mapper.faire_template_file,
+    #                                           sheet_name=sample_mapper.sample_mapping_sheet_name, 
+    #                                           faire_template_df=faire_sample_df_updated)
 
     return sample_mapper, faire_sample_df
 
