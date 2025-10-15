259f17ffc68161ebdf3bb6dee041ed2bad558c30
Brynn Zalmanek <brynn.zalmanek@noaa.gov>
2025-10-15 16:59:34 -0700

    additional functionality

--- utils/taxonomy_table_creator.py
+++ utils/taxonomy_table_creator.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import pandas as pd
 import hashlib
 
+# TODO: Get VerbatimIdentification first because Species column has values like Vexillifera sp_ AKu_2020a, where its an unclassified 
+# species the AKu_2020a is the NCBI identifier. Consider adding the taxonID too. The specificEpithet functionality will drop this info.
 class TaxonomyTableCreator:
     # FAIRE Taxonomy fields
     SEQ_ID = "seq_id"
@@ -12,7 +14,13 @@ class TaxonomyTableCreator:
     ORDER = "order"
     FAMILY = "family"
     GENUS = "genus"
-    SPECIES = "species" # TODO: tehnically species is not in FAIRE for some reason - take out? Or update as userDefined field?
+    SPECIFIC_EPITHET = "specificEpithet"
+    SCIENTIFIC_NAME = "scientificName"
+    SCIENTIFIC_NAME_AUTHORSHIP = "scientificNameAuthorship"
+    TAXON_RANK = "taxonRank"
+
+    TAXONOMY_COLS = [KINGDOM, PHYLUM, CLASS, ORDER, FAMILY, GENUS]
+    TAXONOMY_UNKNOWNS = ["Unknown"] # TODO - REvamp uses Unknown, not sure if there will be other ways of saying this for other dbs.
     
     def __init__(self, taxon_tble_file: str, asv_dna_seq_file: str):
         """
@@ -23,19 +31,9 @@ class TaxonomyTableCreator:
         self.taxon_table_path = Path(taxon_tble_file)
         self.asv_dna_seq_file = Path(asv_dna_seq_file)
         self.hash_dna_seq_dict = self._create_asv_hash_dict()
-        self.taxon_df = self.create_faire_df()
+        self.taxon_df = self.get_faire_df()
     
-
-    def create_faire_df(self) -> pd.DataFrame:
-        """
-        Creates the faire standardized data frame
-        """
-        # 1. Load the data frame
-        df = self.load_taxon_tble_as_df()
-
-        return df
-
-    def load_taxon_tble_as_df(self) -> pd.DataFrame:
+    def get_faire_df(self) -> pd.DataFrame:
         """
         Load the taxon_tble_file as a data frame
         """
@@ -46,6 +44,15 @@ class TaxonomyTableCreator:
 
         # Replace ASVs with hashes
         df_with_hashes = self._switch_asv_for_hashes(df=df_cols_renamed)
+
+        # Fix scientificEpithet column (which is currently just the species column from the original df)
+        df_with_hashes[self.SPECIFIC_EPITHET] = df_with_hashes[self.SPECIFIC_EPITHET].apply(self._get_specific_epithet)
+
+        # Get scientific name
+        df_with_hashes[self.SCIENTIFIC_NAME] = df_with_hashes.apply(self._get_scientific_name, axis=1)
+        
+        # Get taxon_rank
+        df_with_hashes[self.TAXON_RANK] = df_with_hashes.apply(self._get_taxon_rank, axis=1)
 
         return df_with_hashes
     
@@ -121,9 +128,9 @@ class TaxonomyTableCreator:
             elif col.lower() == self.FAMILY:
                 col_renames[col] = self.FAMILY
             elif col.lower() == self.GENUS:
-                col_renames[col] = self.FAMILY
-            elif col.lower() == self.SPECIES:
-                col_renames[col] = self.SPECIES
+                col_renames[col] = self.GENUS
+            elif col.lower() == "species":
+                col_renames[col] = self.SPECIFIC_EPITHET
 
         df.rename(columns=col_renames, inplace=True)
         return df
@@ -141,7 +148,49 @@ class TaxonomyTableCreator:
         df[self.SEQ_ID] = df[self.SEQ_ID].map(hash_map)
         return df
 
+    def _get_taxon_rank(self, row: pd.Series) -> str:
+        """
+        Gets the most specific column name for the most specific
+        name in the Scientific name.
+        """
+        # Iterate through the taxonomic cols in reverse order (most specific first)
+        for col in reversed(self.TAXONOMY_COLS):
+            if pd.notna(row[col]) and row[col] not in self.TAXONOMY_UNKNOWNS:
+                if col == self.SPECIFIC_EPITHET:
+                    return "species"
+                else:
+                    return col
+   
+        return "missing: not provided"
+    
+    def _get_scientific_name(self, row: pd.Series) -> str:
+        """
+        Gets the most scientific name for the most specific 
+        value in the taxonomies.
+        """
+        for col in reversed(self.TAXONOMY_COLS):
+            if pd.notna(row[col]) and row[col] not in self.TAXONOMY_UNKNOWNS:
+                return row[col]
         
+        return "not applicable"
+
+    def _get_specific_epithet(self, species_value: str) -> str:
+        """
+        The specific epithet column is currently just mapped to the species
+        column in the origina taxonomy.txt file, need to edit to fit 
+        the speicicEpithet which is the lower case value of the species, 
+        everything else that is NA will become 'not applicable'
+        """
+        
+        if pd.isna(species_value) or species_value in self.TAXONOMY_UNKNOWNS:
+            return "not applicable"
+        else:
+            species_words = str(species_value).split()
+            if len(species_words) ==2:
+                return species_words[1]
+            else:
+                print(f"There appear to be a weird species structure with value {species_value}. Please check this and update code accordingly")
+                return "not applicable" 
                     
                     
               
