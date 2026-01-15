c2250a0fa95e1dc86861be6c8ad884cabcfc8ab5
Brynn Zalmanek <brynn.zalmanek@noaa.gov>
2026-01-15 15:08:16 -0800

    remove code from wrong main.py file

--- projects/main.py
+++ projects/main.py
@@ -4,21 +4,6 @@ import sys
 sys.path.append("..")
 from utils.project_mapper import ProjectMapper
 
-def fix_zenodo_version(df: pd.DataFrame) -> pd.DataFrame:
-    nucl_acid_ext_and_samp_method_updates = {"https://doi.org/10.5281/zenodo.11398154": "https://doi.org/10.5281/zenodo.17655148",
-                             "https://doi.org/10.5281/zenodo.15793435": "https://doi.org/10.5281/zenodo.17655184",
-                             "https://doi.org/10.5281/zenodo.16850033": "https://doi.org/10.5281/zenodo.17401131",
-                             "https://doi.org/10.5281/zenodo.16740832": "https://doi.org/10.5281/zenodo.17655213",
-                             "https://doi.org/10.5281/zenodo.11398178": "https://doi.org/10.5281/zenodo.17655027",
-                            "https://doi.org/10.5281/zenodo.16755251": "https://doi.org/10.5281/zenodo.17655056"}
-    
-    df_replaced = df.replace(nucl_acid_ext_and_samp_method_updates)
-
-    return df_replaced
-    
-
-
-
 def main() -> None:
 
     parser = argparse.ArgumentParser(description='Process project configuration path for FAIRe mapping of projects for GBIF/OBIS')
